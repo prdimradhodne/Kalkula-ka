8289543781e325982d8daa023feae7aa6cc54d61
svajuznerusimnij <142452216+prdimradhodne@users.noreply.github.com>
2024-01-24 17:32:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//Chcete přidat / odebrat DPH?
+Console.WriteLine("Chcete přidat nebo odebrat DPH? [P/O]");
+string volba = Console.ReadLine().ToLower();
+
+if (volba != "p"&& volba != "o")
+{
+    Console.WriteLine("Chybně zadaný formát...");
+    
+}
+else
+{
+    //Zeptej se uživatele na cenu 
+    Console.WriteLine("Jaká je cena produktu?");
+    int cena = Convert.ToInt32(Console.ReadLine());
+
+    //pdomínka, když uživatel zadá přidat nebo odebrat
+    if (volba == "p")
+    {
+        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 121}");
+        Thread.Sleep(3000);
+    }
+    else if (volba == "o")
+    {
+        Console.WriteLine($"Když je DPH 21 %, tak vaše cena je: {cena / 100 * 79}");
+        Thread.Sleep(3000);
+    }
+}
+
+    
